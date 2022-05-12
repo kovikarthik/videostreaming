71014e3da21e5cd4cdbd2d36cdf378336a805d3c
karthik <60565723+kovikarthik@users.noreply.github.com>
2022-05-12 17:18:17 +0530

    push

---
+++ frontend/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="navbar">
+        <h1>Vasukam Connectivity</h1>
+    </div>
+</template>
+
+<style scoped>
+.navbar{
+    display:flex;
+    background-color: #12d9f4;
+    justify-content: center;
+    align-items: center;
+}
+h1{
+    color:#f23c0e;
+}
+</style>
---
+++ frontend/pages/player/_name.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="player">
+        <video controls autoPlay crossOrigin="anonymous">
+            <source :src="`http://localhost:5000/videos/video/${vidName}`" type="video/mp4">
+            <track style="display:flex;align-items:center;justify-content:center" label="English" kind="captions" srcLang="en" :src="`http://localhost:5000/videos/video/${vidName}/caption`" default>
+        </video>
+    </div>
+</template>
+
+<script>
+export default {
+ data() {
+      return {
+        vidName: ''
+      }
+    },
+mounted(){
+    this.vidName = this.$route.params.name
+}
+}
+</script>
+
+<style scoped>
+.player{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 2em;
+}
+</style>
